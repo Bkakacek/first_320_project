b5fdc95f80279cc7291849e902aaa1e08e3ae5f7
Blake Kakacek <blake.kakacek@my.simpson.edu>
2019-02-12 19:32:43 -0600

    Lab 5 files added to the project.

--- web/js/name_list.js
+++ web/js/name_list.js
@@ -7,12 +7,14 @@ function updateTable() {
             // Here we loop the array and print the first name.
         var tr;
         for (var i = 0; i < json_result.length; i++) {
+                var dashedNumber = json_result[i].phone.substring(0,3) + "-" + json_result[i].phone.substring(3,6) + "-"
+                + json_result[i].phone.substring(6,10);
                 tr = $('<tr/>');
                 tr.append("<td>" + json_result[i].id + "</td>");
                 tr.append("<td>" + json_result[i].first + "</td>");
                 tr.append("<td>" + json_result[i].last + "</td>");
                 tr.append("<td>" + json_result[i].email + "</td>");
-                tr.append("<td>" + json_result[i].phone + "</td>");
+                tr.append("<td>" + dashedNumber + "</td>");
                 tr.append("<td>" + json_result[i].birthday + "</td>");
                 $('table').append(tr);
             }
@@ -21,3 +23,24 @@ function updateTable() {
 }
 
 updateTable();
+
+function showDialogAdd() {
+
+    // Print that we got here
+    console.log("Opening add item dialog");
+
+    // Clear out the values in the form.
+    // Otherwise we'll keep values from when we last
+    // opened or hit edit.
+    // I'm getting it started, you can finish.
+    $('#id').val("");
+    $('#firstName').val("");
+    $('#lastName').val("");
+    $('#email').val("");
+
+    // Show the hidden dialog
+    $('#myModal').modal('show');
+}
+
+var addItemButton = $('#addItem');
+addItemButton.on("click", showDialogAdd);
