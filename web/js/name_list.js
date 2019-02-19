47f3b07aa4fbb194ea4302f4cd62204168e00b19
Blake Kakacek <blake.kakacek@my.simpson.edu>
2019-02-19 17:30:10 -0600

    Lab 5

--- web/js/name_list.js
+++ web/js/name_list.js
@@ -24,23 +24,140 @@ function updateTable() {
 
 updateTable();
 
+// Opens-up the hidden dialog box for user information on mouse click
+var addItemButton = $('#addItem');
+addItemButton.on("click", showDialogAdd);
+
 function showDialogAdd() {
 
     // Print that we got here
     console.log("Opening add item dialog");
 
-    // Clear out the values in the form.
-    // Otherwise we'll keep values from when we last
-    // opened or hit edit.
-    // I'm getting it started, you can finish.
+
     $('#id').val("");
     $('#firstName').val("");
     $('#lastName').val("");
     $('#email').val("");
+    $('#phone').val("");
+    $('#birthday').val("");
+
+    // Clear each field every time the dialog box is opened
+
+    $('#firstName').removeClass("is-valid");
+    $('#lastName').removeClass("is-valid");
+    $('#email').removeClass("is-valid");
+    $('#phone').removeClass("is-valid");
+    $('#birthday').removeClass("is-valid");
+    $('#firstName').removeClass("is-invalid");
+    $('#lastName').removeClass("is-invalid");
+    $('#email').removeClass("is-invalid");
+    $('#phone').removeClass("is-invalid");
+    $('#birthday').removeClass("is-invalid");
 
     // Show the hidden dialog
     $('#myModal').modal('show');
 }
 
-var addItemButton = $('#addItem');
-addItemButton.on("click", showDialogAdd);
+// Validates first name
+function validateFirstNameField() {
+
+    // First name variables
+    var firstNameAttrib = $('#firstName');
+    var firstNameValidate = firstNameAttrib.val();
+
+    // Regular expression found at https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
+    // It allows for special characters in names (apostrophes, hyphens, etc.)
+    var firstNameRegEx = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+
+
+    if (firstNameRegEx.test(firstNameValidate)){
+        firstNameAttrib.removeClass("is-invalid");
+        firstNameAttrib.addClass("is-valid");
+    }
+}
+
+// Validates last name
+function validateLastNameField() {
+
+    // Last name variables
+    var lastNameAttrib = $('#lastName');
+    var lastNameValidate = lastNameAttrib.val()
+
+    // Regular expression found at https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
+    // It allows for special characters in names (apostrophes, hyphens, etc.)
+    var lastNameRegEx = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+
+
+    if (lastNameRegEx.test(lastNameValidate)) {
+        lastNameAttrib.removeClass("is-invalid");
+        lastNameAttrib.addClass("is-valid");
+    }
+}
+
+// Validates email address
+function validateEmailField() {
+
+    //Email variables
+    var emailAttrib = $('#email');
+    var emailValidate = emailAttrib.val();
+    var emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    // Conditional checking the validity of the email field
+    if (emailRegEx.test(emailValidate)) {
+        emailAttrib.removeClass("is-invalid");
+        emailAttrib.addClass("is-valid");
+    }
+}
+
+// Validates phone number
+function validatePhoneField() {
+
+    //Phone number variables
+    var phoneAttrib = $('#phone');
+    var phoneValidate = phoneAttrib.val();
+    var phoneRegEx = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+    // Conditional checking the validity of the phone field
+    if (phoneRegEx.test(phoneValidate)) {
+        phoneAttrib.removeClass("is-invalid");
+        phoneAttrib.addClass("is-valid");
+    }
+}
+
+// Validates birthday
+function validateBirthdayField() {
+
+    // Birthday variables
+    var birthdayAttrib = $('#birthday');
+    var birthdayValidate = birthdayAttrib.val();
+
+    // ***I set-up the regular expression like this because it is how the data
+    // is formatted within the data records, but this probably will be changed later***
+    var birthdayRegEx = /^\(?([0-9]{4})\)?[-. ]?([0-9]{2})[-. ]?([0-9]{2})$/;
+
+    // Conditional checking the validity of the birthday field
+    if (birthdayRegEx.test(birthdayValidate)) {
+        birthdayAttrib.removeClass("is-invalid");
+        birthdayAttrib.addClass("is-valid");
+    }
+
+}
+
+// Executes each function on a button click
+
+var saveChangesButton = $('#saveChanges');
+saveChangesButton.on("click", saveChanges);
+
+function saveChanges() {
+
+        // Calling all functions. It's like assembling the Avengers!!
+        validateFirstNameField();
+        validateLastNameField();
+        validateEmailField();
+        validatePhoneField();
+        validateBirthdayField();
+
+        // Show the console the functions were executed
+        console.log("Changes Saved")
+    }
+
